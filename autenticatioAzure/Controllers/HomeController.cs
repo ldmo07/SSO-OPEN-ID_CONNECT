c4b83ce77d42d4804551ac4a9bae133905b022dd
Luis David <luisdavidmercadoortega@gmail.com>
2024-05-02 17:17:32 -0500

    Subiendo codigo

---
+++ autenticatioAzure/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using autenticatioAzure.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace autenticatioAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            String mensaje = "";
+
+            if (User.Identity.IsAuthenticated)
+                mensaje = "El usuario se encuentra logueado";
+            else
+                mensaje = "El usuario NO se encuentra logueado";
+
+            ViewBag.mensaje = mensaje;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            String mensaje = "";
+
+            if (User.Identity.IsAuthenticated)
+                mensaje = "El usuario se encuentra logueado";
+            else
+                mensaje = "El usuario NO se encuentra logueado";
+
+            ViewBag.mensaje = mensaje;
+
+            // Obtén la información del usuario
+            var user = GetUserFromSomeSource();
+
+            var profileModel = new UserProfileViewModel
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            };
+
+            return View(profileModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Login()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = Url.Action("Privacy", "Home") }, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = Url.Action("Index", "Home") },
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        private UserProfileViewModel GetUserFromSomeSource()
+        {
+            // Obtener el objeto ClaimsPrincipal del usuario autenticado
+            var user = HttpContext.User;
+
+            // Crear un objeto User con la información de las claims
+            var userProfile = new UserProfileViewModel
+            {
+                UserName = user.Identity.Name,
+                Email = user.FindFirst(ClaimTypes.Surname)?.Value,
+                FirstName = user.FindFirst(ClaimTypes.GivenName)?.Value,
+                LastName = user.FindFirst(ClaimTypes.Surname)?.Value
+                // Agregar más propiedades si es necesario
+            };
+
+            return userProfile;
+        }
+    }
+}
---
+++ autenticatioAzure/Program.cs
@@ -0,0 +1,49 @@
+using autenticatioAzure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
